23e5acc8feebc8438e707b683a299df01f185260
MAURÍCIO TELLES SILVA BICHELS <mauricio_bichels@estudante.sesisenai.org.br>
2025-07-08 09:47:18 -0300

    create

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Menu from './componentes/Menu.jsx'
+
+function App() {
+  return (
+      <div>
+        <Menu />
+      </div>
+  )
+}
+
+export default App
---
+++ src/componentes/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const menu = () => {
+  return (
+    <div>
+      <h1>Oi, eu sou um humano(quase um burro)</h1>
+      <h2>Moro em palhoça city</h2>  
+    </div>
+  )
+}
+
+export default menu
